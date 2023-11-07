9b87095395da25e02846627bd4fb1f643b13d2c3
“dan” <dan.mariuta@cerbu.tech>
2023-11-08 01:08:36 +0200

    Premmerce add to sidebar shop

--- vm.sh
+++ vm.sh
@@ -66,6 +66,7 @@ ls -la /var/ansible
 #ansible-playbook /var/ansible/setup.yml --tags pages
 #ansible-playbook /var/ansible/setup.yml --tags shipping
 #ansible-playbook /var/ansible/setup.yml --tags db
-ansible-playbook /var/ansible/setup.yml
+ansible-playbook /var/ansible/setup.yml --tags sidebars
+#ansible-playbook /var/ansible/setup.yml
 
 EOF
