540c4da11e9e4ffba290fec9d2aec4e2046648f1
“dan” <dan.mariuta@cerbu.tech>
2023-11-10 02:33:18 +0200

    Restore stable

--- vm.sh
+++ vm.sh
@@ -18,14 +18,12 @@ sshpass -p "$REMOTE_PASSWORD" scp -o StrictHostKeyChecking=no "$LOCAL_MYSQL_PRIV
 
 # Install Ansible, Git, clone the repository, and clean up
 sshpass -p "$REMOTE_PASSWORD" ssh -o StrictHostKeyChecking=no "$REMOTE_USER@$REMOTE_HOST" GIT_REPO_URL="$GIT_REPO_URL" bash -s << 'EOF'
-lsb_release -a
-
 
 sudo apt-get update
 sudo apt-get install -y software-properties-common
-sudo apt update
-sudo apt install python3-pip -y
-pip3 install ansible
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt-get install -y ansible
+sudo apt-get install -y git
 
 # Configure SSH key for Git operations
 chmod 600 /tmp/private_key
