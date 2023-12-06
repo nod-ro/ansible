aa1e0709e7c4a7e11409b340e2168d1c0b997bbb
“dan” <dan.mariuta@cerbu.tech>
2023-12-06 15:58:25 +0200

    NBP-1394 Check if 4G of swap is not already added before adding it

--- deploy_development.sh
+++ deploy_development.sh
@@ -68,6 +68,7 @@ ls -la /var/ansible
 #ansible-playbook /var/ansible/deploy_development.yml --tags sidebars
 #ansible-playbook /var/ansible/deploy_development.yml --tags contact
 #ansible-playbook /var/ansible/deploy_development.yml --tags options
-ansible-playbook /var/ansible/deploy_development.yml
+ansible-playbook /var/ansible/deploy_development.yml --tags swap
+#ansible-playbook /var/ansible/deploy_development.yml
 
 EOF
