bd611bcb8c19050bbdf7bdc528f4f74f418c8d00
Dan <mariutamihaidan@gmail.com>
2024-03-08 04:13:40 +0200

    Adding upgrade script draft

--- upgrade.sh
+++ upgrade.sh
@@ -50,8 +50,8 @@ sudo su -
 export LC_ALL=C
 export LANG=C
 export LANGUAGE=C
-apt-get update
-apt-get install -y locales
+#apt-get update
+#apt-get install -y locales
 locale-gen en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
