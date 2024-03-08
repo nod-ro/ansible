6605f030c95b6c9e937f212703fcff77b139e59a
Dan <mariutamihaidan@gmail.com>
2024-03-08 04:09:38 +0200

    Adding upgrade script draft

--- upgrade.sh
+++ upgrade.sh
@@ -17,11 +17,11 @@ confirm_action
 WEBSITES_LENGTH=$(yq eval '.websites | length' websites.yml)
 
 for ((i = 0 ; i < $WEBSITES_LENGTH ; i++ )); do
+    DEVELOPMENT_DOMAIN=$(yq eval ".websites[$i].name" websites.yml)
     REMOTE_HOST=$(yq eval ".websites[$i].vm_ip" websites.yml)
     REMOTE_USER=$(yq eval ".websites[$i].vm_user" websites.yml)
     REMOTE_PASSWORD=$(yq eval ".websites[$i].vm_password" websites.yml)
     LOCAL_SSH_PRIVATE_KEY_PATH=$(yq eval '.ssh_key_path' websites.yml)
-    LOCAL_MYSQL_PRIVATE_KEY_PATH=$(yq eval ".websites[$i].local_mysql_cert_path" websites.yml)
     REMOTE_MYSQL_PRIVATE_KEY_PATH=$(yq eval '.remote_mysql_cert_path' websites.yml)
 
     SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -29,35 +29,36 @@ for ((i = 0 ; i < $WEBSITES_LENGTH ; i++ )); do
 
     # Upload SSH private key for Git operations
     sshpass -p "$REMOTE_PASSWORD" scp -o StrictHostKeyChecking=no "$LOCAL_SSH_PRIVATE_KEY_PATH" "$REMOTE_USER@$REMOTE_HOST:/tmp/private_key"
-
 # Install Ansible, Git, clone the repository, and clean up
 sshpass -p "$REMOTE_PASSWORD" ssh -o StrictHostKeyChecking=no "$REMOTE_USER@$REMOTE_HOST" GIT_REPO_URL="$GIT_REPO_URL" bash -s << 'EOF'
-
-# Configure SSH key for Git operations
 chmod 600 /tmp/private_key
 eval $(ssh-agent -s)
 ssh-add /tmp/private_key
 ssh -Tvvv git@github.com
 sudo rm -rf /var/ansible
 sudo git clone $GIT_REPO_URL /var/ansible
-
-# Change ownership of /var/ansible to the remote user
 sudo chown -R $USER:$USER /var/ansible
 
 EOF
-#
-## Upload environment.yml from the local machine to the remote VM
-#sshpass -p "$REMOTE_PASSWORD" scp -o StrictHostKeyChecking=no "$SCRIPT_DIR/environment.yml" "$REMOTE_USER@$REMOTE_HOST:/var/ansible/"
-#
-#sshpass -p  "$REMOTE_PASSWORD" scp -o StrictHostKeyChecking=no "$LOCAL_MYSQL_PRIVATE_KEY_PATH" "$REMOTE_USER@$REMOTE_HOST:/var/ansible/data/mysql_certificate.pem"
-#
-## Connect to the VM and run multiple commands
-#sshpass -p "$REMOTE_PASSWORD" ssh -o StrictHostKeyChecking=no "$REMOTE_USER@$REMOTE_HOST" bash -s << 'EOF'
 
+# Connect to the VM and run multiple commands
+sshpass -p "$REMOTE_PASSWORD" ssh -o StrictHostKeyChecking=no "$REMOTE_USER@$REMOTE_HOST" bash -s << 'EOF'
+
+cd /var/ansible
+ls -la
+sudo su -
 export LC_ALL=C
 export LANG=C
 export LANGUAGE=C
-cd /var/ansible
-ansible-playbook /var/ansible/upgrade.yml
+apt-get update
+apt-get install -y locales
+locale-gen en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+ls -la /var/ansible
 
+ansible-playbook /var/ansible/upgrade.yml -e "development_domain=$DEVELOPMENT_DOMAIN"
+
+EOF
 done
