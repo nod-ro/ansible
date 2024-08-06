ad3990b415c0deaa8ee0ff2b593797cda8a63484
Dan <mariutamihaidan@gmail.com>
2024-08-06 14:49:44 +0300

    Updating deploy development to update mysql certificate directly

--- deploy_development.sh
+++ deploy_development.sh
@@ -42,7 +42,7 @@ EOF
 
 # Upload environment.yml from the local machine to the remote VM
 sshpass -p "$REMOTE_PASSWORD" scp -o StrictHostKeyChecking=no "$SCRIPT_DIR/environment.yml" "$REMOTE_USER@$REMOTE_HOST:/var/ansible/"
-
+sshpass -p "$REMOTE_PASSWORD" ssh -o StrictHostKeyChecking=no "$REMOTE_USER@$REMOTE_HOST" "mkdir -p /var/ansible/data/"
 sshpass -p  "$REMOTE_PASSWORD" scp -o StrictHostKeyChecking=no "$LOCAL_MYSQL_PRIVATE_KEY_PATH" "$REMOTE_USER@$REMOTE_HOST:/var/ansible/data/mysql_certificate.pem"
 
 # Connect to the VM and run multiple commands
